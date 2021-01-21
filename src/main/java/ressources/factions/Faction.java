6d2e589d15d9f50d433b5d3e70d938d36a06f8a0
williamquach <wquach@myges.fr>
2021-01-21 23:53:17 +0100

    Added bribe, winning & losing supporters, increasing & decreasing satisfaction functions in Faction class

--- src/main/java/ressources/factions/Faction.java
+++ src/main/java/ressources/factions/Faction.java
@@ -1,19 +1,69 @@
 package ressources.factions;
 
 public class Faction {
+    private String name;
     private int nbSupporters;
-    private int approvalPercent;
+    private int satisfactionRate;
 
-    public Faction(int nbSupporters, int approvalPercent) {
+    public Faction(int nbSupporters, int satisfactionRate) {
         this.nbSupporters = nbSupporters;
-        this.approvalPercent = approvalPercent;
+        this.satisfactionRate = satisfactionRate;
     }
 
-    public void decreaseNbSupportersBy(int percent) {
-        this.nbSupporters *= 1 - percent/100;
+    public int getNbSupporters() {
+        return this.nbSupporters;
     }
 
-    public void increaseNbSupportersBy(int percent) {
-        this.nbSupporters *= 1 + percent/100;
+    public int getSatisfactionRate() {
+        return this.satisfactionRate;
     }
+
+    public void setNbSupporters(int nbSupporters) {
+        this.nbSupporters = nbSupporters;
+    }
+
+    public void setSatisfactionRate(int newRate) {
+        if(this.satisfactionRate == 0) {
+            System.out.println(String.format("The %s faction is against this republic, it is no longer possible to increase its satisfaction rate.", this.name));
+        }
+        else {
+            if(newRate <= 0) {
+                this.satisfactionRate = 0;
+            }
+            else if(newRate >= 100) {
+                this.satisfactionRate = 100;
+            }
+            else {
+                this.satisfactionRate = newRate;
+            }
+        }
+
+    }
+    
+    public void winsSupporters(int percentage) {
+        setNbSupporters(this.nbSupporters * 1 + percent/100);
+    }
+
+    public void losesSupporters(int percentage) {
+        setSatisfactionPercent(this.nbSupporters * 1 - percent/100);
+    }
+
+    public void increaseSatisfactionBy(int percentagePoint) {
+        setSatisfactionRate(this.nbSupporters + percentagePoint);
+    }
+
+    public void decreaseSatisfactionBy(int percentagePoint) {
+        setSatisfactionRate(this.nbSupporters - percentagePoint);
+    }
+
+    public int getBribePrice() {
+        return getNbSupporters() * 15; // TODO '15' à mettre dans une constante qque part ?
+    }
+
+    public void bribe() {
+        increaseSatisfactionBy(10); // TODO '10' pareil que getBribePrice()
+    }
+
+    // TODO : Ou faire la diminution de l'approbation des loyalistes ? lorsque bribe()
+
 }
