56dc5f5a561ec05f418e5fbca32ff71012ba03d1
Fred <frederic.Flaceliere@gmail.com>
2021-02-08 18:20:29 +0100

    V0.2.3 - refonte pourcentage
    refonte du fonctionnement des pourcentage de l’argiculture et de l’industrie

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,8 +2,12 @@ package Main;
 
 import ressources.economy.Tresory;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+//import json;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Tresory tresory = new Tresory();
         System.out.println("test main");
 
